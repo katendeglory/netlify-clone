dc4052b923c68e1203b68048fa6c8006e87d5ba2
Glory Katende <katendeglory@gmail.com>
2020-06-23 16:42:07 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '80': '20rem',
+        '96': '24rem',
+        '112': '28rem',
+        '120': '30rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
